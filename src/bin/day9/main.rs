6ae07828e2e8ba244389d01ccd855cd010f3d7d4
Mieszko Sabo <mieszko.sabo@icloud.com>
2022-12-12 12:15:41 +0100

    part one

--- src/bin/day9/main.rs
+++ src/bin/day9/main.rs
@@ -14,18 +14,6 @@ fn part_one() -> usize {
     simulation_interpreter(motions)
 }
 
-#[derive(Clone, Copy, Debug)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-    UpRight,
-    DownRight,
-    DownLeft,
-    UpLeft,
-}
-
 #[derive(Eq, Hash, PartialEq, Clone, Copy, Debug)]
 struct Coord(i32, i32);
 
@@ -36,121 +24,41 @@ impl Add for Coord {
     }
 }
 
-impl From<Coord> for HeadTailRelationship {
-    fn from(c: Coord) -> Self {
-        if c.0 == 0 && c.1 == 0 {
-            return HeadTailRelationship::Covers;
-        }
-
-        // adjacent
-        if c.0.abs() <= 1 && c.1.abs() <= 1 {
-            let dir: Direction = c.into();
-            return HeadTailRelationship::Adjacent(dir);
-        }
-
-        // two step
-        let dir: Direction = c.into();
-        HeadTailRelationship::TwoStep(dir)
-    }
-}
-
-impl From<&str> for Direction {
+impl From<&str> for Coord {
     fn from(s: &str) -> Self {
         match s {
-            "R" => Direction::Right,
-            "U" => Direction::Up,
-            "L" => Direction::Left,
-            "D" => Direction::Down,
+            "R" => Coord(1, 0),
+            "U" => Coord(0, 1),
+            "L" => Coord(-1, 0),
+            "D" => Coord(0, -1),
             _ => panic!("unexpected direction"),
         }
     }
 }
 
-impl From<Direction> for Coord {
-    fn from(value: Direction) -> Self {
-        match value {
-            Direction::Up => Coord(0, 1),
-            Direction::Down => Coord(0, -1),
-            Direction::Left => Coord(-1, 0),
-            Direction::Right => Coord(1, 0),
-            Direction::UpRight => Coord(1, 1),
-            Direction::DownRight => Coord(1, -1),
-            Direction::DownLeft => Coord(-1, -1),
-            Direction::UpLeft => Coord(-1, 1),
-        }
-    }
+type HeadTailRelationship = Coord;
+
+type Motion = (Coord, usize);
+
+fn move_head(state: HeadTailRelationship, head_move: Coord) -> HeadTailRelationship {
+    state + head_move
 }
 
-impl From<Coord> for Direction {
-    fn from(c: Coord) -> Self {
-        let normalized = Coord(c.0.signum(), c.1.signum());
-        match normalized {
-            Coord(0, 1) => Direction::Up,
-            Coord(0, -1) => Direction::Down,
-            Coord(-1, 0) => Direction::Left,
-            Coord(1, 0) => Direction::Right,
-            Coord(1, 1) => Direction::UpRight,
-            Coord(1, -1) => Direction::DownRight,
-            Coord(-1, -1) => Direction::DownLeft,
-            Coord(-1, 1) => Direction::UpLeft,
-            _ => unreachable!(),
-        }
-    }
-}
-
-#[derive(Debug)]
-enum HeadTailRelationship {
-    /// H and T are in the same spot
-    Covers,
-
-    /// eg.: or .....
-    /// .....   ..H..
-    /// .TH..   .T...
-    /// .....   .....
-    Adjacent(Direction),
-
-    /// eg.: or
-    /// .....   ...H.
-    /// .....   .....
-    /// .T.H.   ..T..
-    /// .....   .....
-    TwoStep(Direction),
-}
-
-type Motion = (Direction, usize);
-
-fn move_head(state: HeadTailRelationship, head_move: Direction) -> HeadTailRelationship {
-    match state {
-        HeadTailRelationship::Covers => HeadTailRelationship::Adjacent(head_move),
-        HeadTailRelationship::Adjacent(dir) => {
-            let new_coord = Coord::from(dir) + Coord::from(head_move);
-            println!("head move {:?}", new_coord);
-            let dir = new_coord.into();
-            println!("head move dir {:?}", dir);
-
-            dir
-        }
-        HeadTailRelationship::TwoStep(_) => panic!("impossible state"),
-    }
-}
-
-fn move_tail(state: HeadTailRelationship) -> (HeadTailRelationship, Option<Direction>) {
-    match state {
-        HeadTailRelationship::Covers => (HeadTailRelationship::Covers, None),
-        HeadTailRelationship::Adjacent(dir) => (HeadTailRelationship::Adjacent(dir), None),
-        HeadTailRelationship::TwoStep(dir) => match dir {
-            Direction::UpRight => (HeadTailRelationship::Adjacent(Direction::Right), Some(dir)),
-            Direction::DownRight => (HeadTailRelationship::Adjacent(Direction::Right), Some(dir)),
-            Direction::DownLeft => (HeadTailRelationship::Adjacent(Direction::Left), Some(dir)),
-            Direction::UpLeft => (HeadTailRelationship::Adjacent(Direction::Left), Some(dir)),
-            dir => (HeadTailRelationship::Adjacent(dir), Some(dir)),
-        },
+fn move_tail(state: HeadTailRelationship) -> (HeadTailRelationship, Option<Coord>) {
+    if state.0.abs() <= 1 && state.1.abs() <= 1 {
+        (state, None)
+    } else if state.0.abs() > 1 {
+        let tail_move = Some(Coord(state.0.signum(), state.1.signum()));
+        (Coord(state.0.signum(), 0), tail_move)
+    } else {
+        let tail_move = Some(Coord(state.0.signum(), state.1.signum()));
+        (Coord(0, state.1.signum()), tail_move)
     }
 }
 
 fn simulation_interpreter(series: Vec<Motion>) -> usize {
     let mut positions_set: HashSet<Coord> = HashSet::new();
-    let mut current_state = HeadTailRelationship::Covers;
+    let mut current_state = Coord(0, 0);
     let mut current_position = Coord(0, 0);
 
     for (dir, repeat_times) in series.iter() {
@@ -162,7 +70,7 @@ fn simulation_interpreter(series: Vec<Motion>) -> usize {
             current_state = new_state;
 
             if let Some(tail_move) = tail_move {
-                current_position = current_position + tail_move.into();
+                current_position = current_position + tail_move;
                 println!("moved tail {:?}", current_position);
             }
             println!();
